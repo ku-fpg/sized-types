2b765b96cc7147590686018216ca4fc29627e5ab
Andy Gill <andygill@ku.edu>
2009-10-13 15:22:45 -0500

    Adding rotate and shift, and removing the reverse problem

--- Data/Sized/Signed.hs
+++ Data/Sized/Signed.hs
@@ -21,11 +21,11 @@ import Data.Bits
 newtype Signed ix = Signed Integer 
 
 -- 'toMatrix' turns a sized 'Signed' value into a 'Matrix' of 'Bool's. 
-toMatrix :: Size ix => Signed ix -> Matrix ix Bool
-toMatrix s@(Signed v) = matrix $ reverse $ take (bitSize s) $ map odd $ iterate (`div` 2) v
+toMatrix :: (Size ix, Enum ix) => Signed ix -> Matrix ix Bool
+toMatrix s@(Signed v) = matrix $ take (bitSize s) $ map odd $ iterate (`div` 2) v
 
 -- 'toMatrix' turns a a 'Matrix' of 'Bool's into sized 'Signed' value. 
-fromMatrix :: Size ix => Matrix ix Bool -> Signed ix
+fromMatrix :: (Size ix, Enum ix) => Matrix ix Bool -> Signed ix
 fromMatrix m = mkSigned $
 	  sum [ n	
 	      | (n,b) <- zip (iterate (* 2) 1)
@@ -33,7 +33,7 @@ fromMatrix m = mkSigned $
 	      , b
 	      ]
 -- 
-mkSigned :: (Size ix) => Integer -> Signed ix
+mkSigned :: (Size ix, Enum ix) => Integer -> Signed ix
 mkSigned v = res
    where sz' = 2 ^ (fromIntegral bitCount :: Integer)
 	 bitCount = bitSize res - 1
@@ -41,30 +41,30 @@ mkSigned v = res
 	  	(s,v') | even s    -> Signed v' 
 		       | otherwise -> Signed (v' - sz') 
 
-instance (Size ix) => Eq (Signed ix) where
+instance (Size ix, Enum ix) => Eq (Signed ix) where
 	(Signed a) == (Signed b) = a == b
-instance (Size ix) => Ord (Signed ix) where
+instance (Size ix, Enum ix) => Ord (Signed ix) where
 	(Signed a) `compare` (Signed b) = a `compare` b
-instance (Size ix) => Show (Signed ix) where
+instance (Size ix, Enum ix) => Show (Signed ix) where
 	show (Signed a) = show a
-instance (Size ix) => Integral (Signed ix) where
+instance (Size ix, Enum ix) => Integral (Signed ix) where
   	toInteger (Signed m) = m
 	quotRem (Signed a) (Signed b) = 
 		case quotRem a b of
 		   (q,r) -> (mkSigned q,mkSigned r)
-instance (Size ix) => Num (Signed ix) where
+instance (Size ix, Enum ix) => Num (Signed ix) where
 	(Signed a) + (Signed b) = mkSigned $ a + b
 	(Signed a) - (Signed b) = mkSigned $ a - b
 	(Signed a) * (Signed b) = mkSigned $ a * b
 	abs (Signed n) = mkSigned $ abs n
 	signum (Signed n) = mkSigned $ signum n
 	fromInteger n = mkSigned n
-instance (Size ix) => Real (Signed ix) where
+instance (Size ix, Enum ix) => Real (Signed ix) where
 	toRational (Signed n) = toRational n
-instance (Size ix) => Enum (Signed ix) where
+instance (Size ix, Enum ix) => Enum (Signed ix) where
 	fromEnum (Signed n) = fromEnum n
 	toEnum n = mkSigned (toInteger n)	
-instance (Size ix) => Bits (Signed ix) where
+instance (Size ix, Enum ix) => Bits (Signed ix) where
 	bitSize s = f s undefined
 	  where
 		f :: (Size a) => Signed a -> a -> Int
@@ -74,6 +74,10 @@ instance (Size ix) => Bits (Signed ix) where
 	a `xor` b = fromMatrix (M.zipWith (/=) (toMatrix a) (toMatrix b))
 	a .|. b = fromMatrix (M.zipWith (||) (toMatrix a) (toMatrix b))
 	a .&. b = fromMatrix (M.zipWith (&&) (toMatrix a) (toMatrix b))
+	shiftL (Signed v) i = mkSigned (v * (2 ^ i))
+	shiftR (Signed v) i = mkSigned (v `div` (2 ^ i))
+ 	rotate v i = fromMatrix (forAll $ \ ix -> m ! (toEnum ((fromEnum ix - i) `mod` M.length m)))
+		where m = toMatrix v
 		
 
 	
--- Data/Sized/Unsigned.hs
+++ Data/Sized/Unsigned.hs
@@ -21,10 +21,10 @@ import Data.Bits
 
 newtype Unsigned ix = Unsigned Integer 
 
-toMatrix :: Size ix => Unsigned ix -> Matrix ix Bool
-toMatrix s@(Unsigned v) = matrix $ reverse $ take (bitSize s) $ map odd $ iterate (`div` 2) v
+toMatrix :: (Size ix, Enum ix) => Unsigned ix -> Matrix ix Bool
+toMatrix s@(Unsigned v) = matrix $ take (bitSize s) $ map odd $ iterate (`div` 2) v
 
-fromMatrix :: Size ix => Matrix ix Bool -> Unsigned ix
+fromMatrix :: (Size ix, Enum ix) => Matrix ix Bool -> Unsigned ix
 fromMatrix m = mkUnsigned $
 	  sum [ n	
 	      | (n,b) <- zip (iterate (* 2) 1)
@@ -32,36 +32,36 @@ fromMatrix m = mkUnsigned $
 	      , b
 	      ]
 
-mkUnsigned :: (Size ix) => Integer -> Unsigned ix
+mkUnsigned :: (Size ix,  Enum ix) => Integer -> Unsigned ix
 mkUnsigned v = res
    where sz' = 2 ^ (fromIntegral bitCount :: Integer)
 	 bitCount = bitSize res
 	 res = Unsigned (v `mod` sz')
 
-instance (Size ix) => Eq (Unsigned ix) where
+instance (Size ix, Enum ix) => Eq (Unsigned ix) where
 	(Unsigned a) == (Unsigned b) = a == b
-instance (Size ix) => Ord (Unsigned ix) where
+instance (Size ix, Enum ix) => Ord (Unsigned ix) where
 	(Unsigned a) `compare` (Unsigned b) = a `compare` b
-instance (Size ix) => Show (Unsigned ix) where
+instance (Size ix, Enum ix) => Show (Unsigned ix) where
 	show (Unsigned a) = show a
-instance (Size ix) => Integral (Unsigned ix) where
+instance (Size ix, Enum ix) => Integral (Unsigned ix) where
   	toInteger (Unsigned m) = m
 	quotRem (Unsigned a) (Unsigned b) = 
 		case quotRem a b of
 		   (q,r) -> (mkUnsigned q,mkUnsigned r)
-instance (Size ix) => Num (Unsigned ix) where
+instance (Size ix, Enum ix) => Num (Unsigned ix) where
 	(Unsigned a) + (Unsigned b) = mkUnsigned $ a + b
 	(Unsigned a) - (Unsigned b) = mkUnsigned $ a - b
 	(Unsigned a) * (Unsigned b) = mkUnsigned $ a * b
 	abs (Unsigned n) = mkUnsigned $ abs n
 	signum (Unsigned n) = mkUnsigned $ signum n
 	fromInteger n = mkUnsigned n
-instance (Size ix) => Real (Unsigned ix) where
+instance (Size ix, Enum ix) => Real (Unsigned ix) where
 	toRational (Unsigned n) = toRational n
-instance (Size ix) => Enum (Unsigned ix) where
+instance (Size ix, Enum ix) => Enum (Unsigned ix) where
 	fromEnum (Unsigned n) = fromEnum n
 	toEnum n = mkUnsigned (toInteger n)	
-instance (Size ix) => Bits (Unsigned ix) where
+instance (Size ix, Enum ix) => Bits (Unsigned ix) where
 	bitSize s = f s undefined
 	  where
 		f :: (Size a) => Unsigned a -> a -> Int
@@ -71,6 +71,10 @@ instance (Size ix) => Bits (Unsigned ix) where
 	a `xor` b = fromMatrix (M.zipWith (/=) (toMatrix a) (toMatrix b))
 	a .|. b = fromMatrix (M.zipWith (||) (toMatrix a) (toMatrix b))
 	a .&. b = fromMatrix (M.zipWith (&&) (toMatrix a) (toMatrix b))
+	shiftL (Unsigned v) i = mkUnsigned (v * (2 ^ i))
+	shiftR (Unsigned v) i = mkUnsigned (v `div` (2 ^ i))
+ 	rotate v i = fromMatrix (forAll $ \ ix -> m ! (toEnum ((fromEnum ix - i) `mod` M.length m)))
+		where m = toMatrix v
 
 -- | common; numerically boolean.		
 type U1 = Unsigned X1
