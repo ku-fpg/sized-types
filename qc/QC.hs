8579703725888dd0f64a966daecb2afa146109b4
Andy Gill <andygill@ku.edu>
2009-10-19 16:00:51 -0500

    Adding together QC into single offline location

---
+++ qc/QC.hs
@@ -0,0 +1,21 @@
+module Data.Sized.QC where
+
+import qualified Test.QuickCheck as QC
+import Data.Sized.Ix
+import Data.Sized.Matrix as M
+import Data.Sized.Arith
+import Data.Sized.Signed as S
+import Data.Sized.Unsigned as U
+
+instance Size n => QC.Arbitrary (X0_ n) where
+	arbitrary = QC.elements [minBound .. maxBound]
+	
+instance Size n => QC.Arbitrary (X1_ n) where
+	arbitrary = QC.elements [minBound .. maxBound]	
+
+instance (QC.Arbitrary ix, Size ix, QC.Arbitrary a) => QC.Arbitrary (Matrix ix a) where
+	arbitrary = f $ \ ixs -> do
+          elems <- sequence [ QC.arbitrary | _ <- ixs ]
+          return $ matrix elems
+         where f :: (Size ix) => ([ix] -> m (Matrix ix a)) -> m (Matrix ix a)
+               f fn = fn M.all
