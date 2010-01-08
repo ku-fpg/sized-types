7a4884f79ecd2c017cb300bf23fb64014fba1c3d
Tristan Bull <tristan@lappy.(none)>
2010-01-08 15:34:46 -0600

    Working implementation of subMatrix and conversions to/from vectors.

--- Hack.hs
+++ Hack.hs
@@ -7,6 +7,8 @@ import Data.Sized.Ix hiding (size)
 import Data.Sized.Vector as V
 import Data.Array as A hiding (indices,(!), ixmap, assocs)
 import qualified Data.List as L
+import Data.Maybe (fromJust)
+import Debug.Trace
 
 x :: Matrix (X2,X3) Int
 x = matrix [1..6]
@@ -25,15 +27,19 @@ data SubMatrix a = SplitH (SubMatrix a) (SubMatrix a)
                     | SplitV (SubMatrix a) (SubMatrix a)
                     | NonZero Int (Vector Int a)
                     | Zero Int
+                 deriving Show
 
 -- matrixtoSubMatrix should take a matrix and split it 
 vectorToSubMatrix v s | pleaseSplitV = SplitV (vectorToSubMatrix vLeft s) (vectorToSubMatrix vRight s)
                       | pleaseSplitH = SplitH (vectorToSubMatrix vTop s) (vectorToSubMatrix vBottom s)
-                      | otherwise   = NonZero numRows (fromIdentity v)
+                      | isZeros v    = Zero s
+--                      | trace (show ("argh")) False = undefined
+                      | otherwise    = NonZero shiftBy (fromIdentity v shiftBy)
     where pleaseSplitV = numCols > s
           pleaseSplitH = numRows > s
-          (vLeft,vRight) = splitVectorV v (numCols `div` 2)
-          (vTop,vBottom) = splitVectorH v (numRows `div` 2)
+          shiftBy = fromJust $ L.findIndex (/= 0) $ toList v
+          (vLeft,vRight) = splitVectorV v (numCols `div` 2 `div` s * s)
+          (vTop,vBottom) = splitVectorH v (numRows `div` 2 `div` s * s)
           (numRows,numCols) = size v
 
 splitVectorV v col | col < numCols = (vLeft,vRight)
@@ -60,10 +66,16 @@ toIdentity v = vector (ix,ix) [ if x == y then v ! x else 0 | x <- ids, y <- ids
     where ix = size v
           ids = indices v
 
-fromIdentity :: (Bounds ix, Num a) => Vector (ix,ix) a -> Vector ix a
-fromIdentity v = vector ix [ v ! (x,y) | (x,y) <- indices v, x == y]
+fromIdentity :: (Bounds ix, Integral ix, Num a) => Vector (ix,ix) a -> ix -> Vector ix a
+fromIdentity v s = vector ix [ v ! (x,(y+s) `mod` ix) | (x,y) <- indices v, x == y]
     where (ix,_) = size v
 
+isIdentity :: (Bounds ix, Num a) => Vector (ix,ix) a -> Bool
+isIdentity v = sum [v ! (x,y) | (x,y) <- indices v, x /= y] == 0 && (xmax == ymax)
+    where (xmax,ymax) = size v
+
+isZeros :: (Bounds ix, Num a) => Vector ix a -> Bool
+isZeros v = sum (toList v) == 0
 
 zeros :: (Bounds ix, Num a) => ix -> Vector ix a
 zeros ix  = vector ix (repeat 0)
@@ -73,5 +85,30 @@ shiftH :: (Bounds x, Bounds y, Integral y) =>
 shiftH n v = ixmap (xmax,ymax) (\(a,b) -> (a,(b - n) `mod` ymax)) v
     where (xmax,ymax) = size v
 
--- shift' x y | x + y > maxBound = (x + y) - maxBound - 1
---            | otherwise        = x + y
+vecEqualTo :: (Bounds ix, Eq a) => Vector (ix,ix) a -> Vector (ix,ix) a -> (Bool,[(ix,ix)])
+vecEqualTo v1 v2 = (numElemsEq,contentsEq)
+    where numElemsEq = x1 == x2 && y1 == y2
+          (x1,y1) = size v1
+          (x2,y2) = size v2
+          contentsEq = [(x,y) | (x,y) <- indices v1, (v1 ! (x,y)) /= (v2 ! (x,y))]
+
+
+main = do
+  g <- readH
+--  let var = vectorToSubMatrix g 32
+  let g' = subMatrixToVector $ vectorToSubMatrix g 32
+--  let gstr = unlines $ map unwords $ toList $ fmap toList $ rows $ fmap show g
+-- let gstr'= unlines $ map unwords $ toList $ fmap toList $ rows $ fmap show g'
+--   writeFile "gfst" gstr
+--   writeFile "gsnd" gstr'
+--   print var
+  let (num,ids) = g `vecEqualTo` g'
+  print (num,ids)
+
+-- foo v = toList $ fmap toList $ rows $ fmap show v
+
+readH :: IO (Vector (Int,Int) Int)
+readH = do
+	str <- readFile "Hmat.csv" 
+	let wds = (map read (words str)) :: [Int]
+	return $ vector (384,1408) wds
