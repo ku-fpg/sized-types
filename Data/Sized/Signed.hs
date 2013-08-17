03ffbb1dc462834999e61b5b0f73dfcb036b01c8
Nicolas Frisby <nicolas.frisby@gmail.com>
2013-08-17 14:39:24 -0500

    generalized the Signed representation type

--- Data/Sized/Signed.hs
+++ Data/Sized/Signed.hs
@@ -25,15 +25,26 @@ import Data.Sized.Fin
 import Data.Bits
 import GHC.TypeLits
 
-newtype Signed (ix :: Nat) = Signed Integer
-    deriving (Eq, Ord)
+type Signed (ix::Nat) = Signed' ix Integer
+
+newtype Signed' (ix :: Nat) a = Signed a deriving (Eq, Ord)
+
+-- specialise to Integer
+{-# SPECIALISE toVector   :: (SingI ix) => Signed ix -> Vector ix Bool #-}
+{-# SPECIALISE fromVector :: (SingI ix) => Vector ix Bool -> Signed ix #-}
+{-# SPECIALISE mkSigned   :: (SingI ix) => Integer -> Signed ix #-}
+
+-- specialise to Int
+{-# SPECIALISE toVector   :: (SingI ix) => Signed' ix Int -> Vector ix Bool #-}
+{-# SPECIALISE fromVector :: (SingI ix) => Vector ix Bool -> Signed' ix Int #-}
+{-# SPECIALISE mkSigned   :: (SingI ix) => Int -> Signed' ix Int #-}
 
 -- 'toVector' turns a sized 'Signed' value into a 'Vector' of 'Bool's.
-toVector :: forall ix . (SingI ix) => Signed ix -> Vector ix Bool
+toVector :: forall ix a. (SingI ix,Integral a) => Signed' ix a -> Vector ix Bool
 toVector (Signed v) = matrix $ take (fromIntegral $ fromSing (sing :: Sing ix)) $ map odd $ iterate (`div` 2) v
 
 -- 'fromVector' turns a 'Vector' of 'Bool's into a sized 'Signed' value.
-fromVector :: (SingI ix) => Vector ix Bool -> Signed ix
+fromVector :: (SingI ix,Integral a) => Vector ix Bool -> Signed' ix a
 fromVector m = mkSigned $
 	  sum [ n
 	      | (n,b) <- zip (iterate (* 2) 1)
@@ -41,43 +52,57 @@ fromVector m = mkSigned $
 	      , b
 	      ]
 --
-mkSigned :: forall ix . (SingI ix) => Integer -> Signed ix
+mkSigned :: forall ix a. (SingI ix,Integral a) => a -> Signed' ix a
 mkSigned v = res
     where sz' = 2 ^ bitCount
-          bitCount :: Integer
+          bitCount :: a
 	  bitCount =  fromIntegral (fromNat (sing :: Sing ix) - 1)
 	  res = case divMod v sz' of
 	  	  (s,v') | even s    -> Signed v'
 		         | otherwise -> Signed (v' - sz')
 
-instance (SingI ix) => Show (Signed ix) where
+instance (SingI ix,Show a) => Show (Signed' ix a) where
+        {-# SPECIALISE instance SingI ix => Show (Signed ix) #-}
+        {-# SPECIALISE instance SingI ix => Show (Signed' ix Int) #-}
 	show (Signed a) = show a
 
-instance (SingI ix) => Read (Signed ix) where
+instance (SingI ix,Read a,Integral a) => Read (Signed' ix a) where
+        {-# SPECIALISE instance (SingI ix) => Read (Signed ix) #-}
+        {-# SPECIALISE instance (SingI ix) => Read (Signed' ix Int) #-}
 	readsPrec i str = [ (mkSigned a,r) | (a,r) <- readsPrec i str ]
 
-instance (SingI ix) => Integral (Signed ix) where
-  	toInteger (Signed m) = m
+instance (SingI ix,Integral a) => Integral (Signed' ix a) where
+        {-# SPECIALISE instance (SingI ix) => Integral (Signed ix) #-}
+        {-# SPECIALISE instance (SingI ix) => Integral (Signed' ix Int) #-}
+  	toInteger (Signed m) = toInteger m
 	quotRem (Signed a) (Signed b) =
 		case quotRem a b of
 		   (q,r) -> (mkSigned q,mkSigned r)
 
-instance (SingI ix) => Num (Signed ix) where
+instance (SingI ix,Num a,Integral a) => Num (Signed' ix a) where
+        {-# SPECIALISE instance (SingI ix) => Num (Signed ix) #-}
+        {-# SPECIALISE instance (SingI ix) => Num (Signed' ix Int) #-}
 	(Signed a) + (Signed b) = mkSigned $ a + b
 	(Signed a) - (Signed b) = mkSigned $ a - b
 	(Signed a) * (Signed b) = mkSigned $ a * b
 	abs (Signed n) = mkSigned $ abs n
 	signum (Signed n) = mkSigned $ signum n
-	fromInteger n = mkSigned n
+	fromInteger n = mkSigned (fromInteger n)
 
-instance (SingI ix) => Real (Signed ix) where
+instance (SingI ix,Integral a) => Real (Signed' ix a) where
+        {-# SPECIALISE instance (SingI ix) => Real (Signed ix) #-}
+        {-# SPECIALISE instance (SingI ix) => Real (Signed' ix Int) #-}
 	toRational (Signed n) = toRational n
 
-instance (SingI ix) => Enum (Signed ix) where
+instance (SingI ix,Integral a) => Enum (Signed' ix a) where
+        {-# SPECIALISE instance (SingI ix) => Enum (Signed ix) #-}
+        {-# SPECIALISE instance (SingI ix) => Enum (Signed' ix Int) #-}
 	fromEnum (Signed n) = fromEnum n
-	toEnum n = mkSigned (toInteger n)
+	toEnum n = mkSigned (toEnum n)
 
-instance (SingI ix) => Bits (Signed ix) where
+instance (SingI ix,Eq a,Bits a,Integral a) => Bits (Signed' ix a) where
+        {-# SPECIALISE instance (SingI ix) => Bits (Signed ix) #-}
+        {-# SPECIALISE instance (SingI ix) => Bits (Signed' ix Int) #-}
 	bitSize _ = fromIntegral (fromNat (sing :: Sing ix))
 
 	complement (Signed v) = Signed (complement v)
@@ -96,41 +121,43 @@ instance (SingI ix) => Bits (Signed ix) where
         popCount n = sum $ fmap (\ b -> if b then 1 else 0) $ elems $ toVector n
 
 
-instance forall ix . (SingI ix) => Bounded (Signed ix) where
+instance forall ix a. (SingI ix,Num a) => Bounded (Signed' ix a) where
+        {-# SPECIALISE instance forall ix. (SingI ix) => Bounded (Signed ix) #-}
+        {-# SPECIALISE instance forall ix. (SingI ix) => Bounded (Signed' ix Int) #-}
 	minBound = Signed (- maxMagnitude)
             where maxMagnitude = 2 ^ (fromNat (sing :: Sing ix) - 1)
         maxBound = Signed (maxMagnitude - 1)
             where maxMagnitude = 2 ^ (fromNat (sing :: Sing ix) - 1)
 
 
-type S2 = Signed 2
-type S3 = Signed 3
-type S4 = Signed 4
-type S5 = Signed 5
-type S6 = Signed 6
-type S7 = Signed 7
-type S8 = Signed 8
-type S9 = Signed 9
-type S10 = Signed 10
-type S11 = Signed 11
-type S12 = Signed 12
-type S13 = Signed 13
-type S14 = Signed 14
-type S15 = Signed 15
-type S16 = Signed 16
-type S17 = Signed 17
-type S18 = Signed 18
-type S19 = Signed 19
-type S20 = Signed 20
-type S21 = Signed 21
-type S22 = Signed 22
-type S23 = Signed 23
-type S24 = Signed 24
-type S25 = Signed 25
-type S26 = Signed 26
-type S27 = Signed 27
-type S28 = Signed 28
-type S29 = Signed 29
-type S30 = Signed 30
-type S31 = Signed 31
-type S32 = Signed 32
+type S2 = Signed' 2 Int
+type S3 = Signed' 3 Int
+type S4 = Signed' 4 Int
+type S5 = Signed' 5 Int
+type S6 = Signed' 6 Int
+type S7 = Signed' 7 Int
+type S8 = Signed' 8 Int
+type S9 = Signed' 9 Int
+type S10 = Signed' 10 Int
+type S11 = Signed' 11 Int
+type S12 = Signed' 12 Int
+type S13 = Signed' 13 Int
+type S14 = Signed' 14 Int
+type S15 = Signed' 15 Int
+type S16 = Signed' 16 Int
+type S17 = Signed' 17 Int
+type S18 = Signed' 18 Int
+type S19 = Signed' 19 Int
+type S20 = Signed' 20 Int
+type S21 = Signed' 21 Int
+type S22 = Signed' 22 Int
+type S23 = Signed' 23 Int
+type S24 = Signed' 24 Int
+type S25 = Signed' 25 Int
+type S26 = Signed' 26 Int
+type S27 = Signed' 27 Int
+type S28 = Signed' 28 Int
+type S29 = Signed' 29 Int
+type S30 = Signed' 30 Int
+type S31 = Signed' 31 Int
+type S32 = Signed' 32 Int
